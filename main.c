64a5877ee92733c712c84d68575844126199875d
andremsouza <msouza.andre@hotmail.com>
2017-07-02 11:47:51 -0300

    Implemented operation requirements. See output

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ char *readLine(FILE *stream) {
 
 int isnumber(char c) {
 	return c == '0' || c == '1' || c == '2' || c == '3' || c == '4' ||
-		c == '5' || c == '6' || c == '7' || c == '8' || c == '9';
+			c == '5' || c == '6' || c == '7' || c == '8' || c == '9';
 }
 
 int isNumber(char* str) {
@@ -105,7 +105,7 @@ record_p read_record_main(FILE *stream) {
 }
 
 int main(int argc, char *argv[]) {
-	int exit, func, ticket;
+	int exit, func, ticket, check;
 	record_p record;
 	char a[200];
 
@@ -113,86 +113,99 @@ int main(int argc, char *argv[]) {
 	//system("clear");
 
 	exit = 0;
+	check = 0; //checagem de requisitos de operacoes
 	while(!exit) {
 		printf("1. Importar arquivo de dados.\n2. Gerar arquivos de indice.\n3. Remover registro.\n4. Inserir registro.\n5. Estatisticas sobre os arquivos de indice\n6. Estatisticas sobre as listas de registros removidos\n7. Sair\n8. Imprime todos os registros.\n\nOpcao: ");
 		scanf("%d", &func);
+		if(func > 1 && func != 7){
+			if(check == 0){
+				printf("\n\nErro: Importe o arquivo de dados antes de executar essa operação.\n\n");
+				continue;
+			}
+			else if(check == 1 && func != 2){
+				printf("\n\nErro: Gere os arquivos de indice antes de executar essa operacao.\n\n");
+				continue;
+			}
+		}
 		switch(func) {
-			case 1:	// importar arquivo de dados
-				import_csv_file(csvPath, file1Path);
-				import_csv_file(csvPath, file2Path);
-				import_csv_file(csvPath, file3Path);
-				printf("\nArquivo de dados importado com sucesso. 3 Arquivos de saida foram gerados.\n\n");
+		case 1:	// importar arquivo de dados
+			check = 1;
+			import_csv_file(csvPath, file1Path);
+			import_csv_file(csvPath, file2Path);
+			import_csv_file(csvPath, file3Path);
+			printf("\nArquivo de dados importado com sucesso. 3 Arquivos de saida foram gerados.\n\n");
 			break;
 
-			case 2:	// gerar arquivos de indice
-				create_index(index1Path, file1Path);
-				create_index(index2Path, file2Path);
-				create_index(index3Path, file3Path);
-				printf("\nArquivos de indice gerados com sucesso.\n\n");
+		case 2:	// gerar arquivos de indice
+			check = 2;
+			create_index(index1Path, file1Path);
+			create_index(index2Path, file2Path);
+			create_index(index3Path, file3Path);
+			printf("\nArquivos de indice gerados com sucesso.\n\n");
 			break;
 
-			case 3:	// remover registro
-				printf("Digite o ticket do registro a ser removido: ");
-				scanf("%d", &ticket);
-				if (remove_record(file1Path, index1Path, ticket) != -1) {
-					remove_record(file2Path, index2Path, ticket);
-					remove_record(file3Path, index3Path, ticket);
-					printf("\nRegistro removido com sucesso.\n\n");
-				} else {
-					printf("\nRegistro nao encontrado.\n\n");
-				}
+		case 3:	// remover registro
+			printf("Digite o ticket do registro a ser removido: ");
+			scanf("%d", &ticket);
+			if (remove_record(file1Path, index1Path, ticket) != -1) {
+				remove_record(file2Path, index2Path, ticket);
+				remove_record(file3Path, index3Path, ticket);
+				printf("\nRegistro removido com sucesso.\n\n");
+			} else {
+				printf("\nRegistro nao encontrado.\n\n");
+			}
 			break;
 
 			// TODO
-			case 4:	// inserir registro
-				while(fgetc(stdin) != '\n');
-				record = read_record_main(stdin);
+		case 4:	// inserir registro
+			while(fgetc(stdin) != '\n');
+			record = read_record_main(stdin);
 
-				if (insert_first_fit(file1Path, index1Path, record) != -1) {
-					insert_best_fit(file2Path, index2Path, record);
-					insert_worst_fit(file3Path, index3Path, record);
-					printf("\nRegistro inserido com sucesso.\n\n");
-				} else {
-					printf("\nRegistro com esse ticket ja existe.\n\n");
-				}
-
-				free_record(record);
-
-			break;
-
-			case 5:	// visualizar estatisticas sobre os arquivos de indice
-			{
-				int c;
-				do {
-					while ((c = fgetc(stdin)) != '\n')
-						;
-					printf("\nVisualizar um por vez? (s/n): ");
-					c = fgetc(stdin);
-				} while (c != 's' && c != 'n');
-
-				if (c == 's')
-					navigate_indices(index1Path, index2Path, index3Path);
-				else if (c == 'n')
-					compare_indices(index1Path, index2Path, index3Path);
-
-				break;
+			if (insert_first_fit(file1Path, index1Path, record) != -1) {
+				insert_best_fit(file2Path, index2Path, record);
+				insert_worst_fit(file3Path, index3Path, record);
+				printf("\nRegistro inserido com sucesso.\n\n");
+			} else {
+				printf("\nRegistro com esse ticket ja existe.\n\n");
 			}
 
-			case 6:	// visualizar estatisticas sobre as listas de registros removidos
-				print_data_file_header_record(file1Path, file2Path, file3Path);
-				// espera enter para retornar ao menu
-				while(fgetc(stdin) != 10);
-				printf("Pressione enter para retornar ao menu.\n");
-				scanf("%[^\n]s", a);
+			free_record(record);
+
 			break;
 
-			case 7:
-				printf("Programa finalizado.\n");
-				exit = 1;
+		case 5:	// visualizar estatisticas sobre os arquivos de indice
+		{
+			int c;
+			do {
+				while ((c = fgetc(stdin)) != '\n')
+					;
+				printf("\nVisualizar um por vez? (s/n): ");
+				c = fgetc(stdin);
+			} while (c != 's' && c != 'n');
+
+			if (c == 's')
+				navigate_indices(index1Path, index2Path, index3Path);
+			else if (c == 'n')
+				compare_indices(index1Path, index2Path, index3Path);
+
+			break;
+		}
+
+		case 6:	// visualizar estatisticas sobre as listas de registros removidos
+			print_data_file_header_record(file1Path, file2Path, file3Path);
+			// espera enter para retornar ao menu
+			while(fgetc(stdin) != 10);
+			printf("Pressione enter para retornar ao menu.\n");
+			scanf("%[^\n]s", a);
 			break;
 
-			case 8:
-				print_all_records(file1Path);
+		case 7:
+			printf("Programa finalizado.\n");
+			exit = 1;
+			break;
+
+		case 8:
+			print_all_records(file1Path);
 			break;
 
 			// df: pergunta de novo
